0eff06466e9c94454732f2834e3e977922d8187a
zakaria ahrbil <zakariaahr@gmail.com>
2025-12-08 13:05:52 +0100

    First commit + setup keycloak + setup db

---
+++ src/main/java/org/zalmoxis/evetic/EveticApplication.java
@@ -0,0 +1,14 @@
+package org.zalmoxis.evetic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveticApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EveticApplication.class, args);
+    }
+}
---
+++ src/test/java/org/zalmoxis/evetic/EveticApplicationTests.java
@@ -0,0 +1,14 @@
+package org.zalmoxis.evetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveticApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+}
