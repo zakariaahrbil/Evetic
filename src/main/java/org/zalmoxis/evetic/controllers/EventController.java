ebaed00a4f220403440c874f2094fffd15e7c54b
zakaria ahrbil <zakariaahr@gmail.com>
2025-12-13 15:53:39 +0100

    Enable method security for event controller and restrict access to organizers

--- src/main/java/org/zalmoxis/evetic/controllers/EventController.java
+++ src/main/java/org/zalmoxis/evetic/controllers/EventController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,6 +30,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/events")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ORGANIZER')")
 public class EventController    
 {
     private final EventService eventService;
